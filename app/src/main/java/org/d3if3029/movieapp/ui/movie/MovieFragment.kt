966f0c270e599fb1634dd02aa13687f76f189a4e
Putri Najah Hidayani <pnajahhidayanidex@gmail.com>
2023-05-14 11:35:46 +0700

    Bikin Fragment movie sama detail

---
+++ app/src/main/java/org/d3if3029/movieapp/ui/movie/MovieFragment.kt
@@ -0,0 +1,24 @@
+package org.d3if3029.movieapp.ui.movie
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.d3if3029.movieapp.R
+
+class MovieFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie, container, false)
+    }
+
+}
